956e7562401177cd34626219262251ae4995c0dc
Mark Drew <mark@distrokid.com>
2025-06-04 13:13:18 +0100

    #1 Adding path mappings (like in CFML) so that packages match what we expect.  There are "lots" of changes just to keep librarties up to date and to enable unit testing.  Also there is formatting enabled so it's all been formatted.

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,57 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 /**
  * Determines if the provided text is eligible for code completion to trigger.
  */
 export function isTextEligibleForCodeCompletion(text: string): boolean {
-	const patterns = [
-		/new $/,
-		/public $/,
-		/private $/,
-		/package $/,
-		/required $/,
-		/\t+, $/, // multi-line optional parameter
-		/.+ function \w+ ?\($/, // begin function prototype optional parameter
-		/.+ function \w+ ?\(.+, $/ // specifying optional function parameter
-	];
+  const patterns = [
+    /new $/,
+    /public $/,
+    /private $/,
+    /package $/,
+    /required $/,
+    /\t+, $/, // multi-line optional parameter
+    /.+ function \w+ ?\($/, // begin function prototype optional parameter
+    /.+ function \w+ ?\(.+, $/, // specifying optional function parameter
+  ];
 
-	return patterns.some(p => text.match(p));
+  return patterns.some((p) => text.match(p));
 }
+
+interface PathMapping {
+				source: string;
+				target: string;
+			}
+
+interface ExtensionConfiguration {
+  pathMappings: PathMapping[];
+}
+
+/**
+ * Applies path mappings to a given relative file path.
+ *
+ * This function retrieves path mappings from the VSCode configuration for "testbox"
+ * and replaces the start of the relative file path with the corresponding target path
+ * if a matching source path is found.
+ *
+ * @param {string} relativeFilePath - The relative file path to apply mappings to.
+ * @returns {string} - The new file path after applying the mappings, or the original
+ *                     relative file path if no mappings match.
+ */
+function applyPathMappings( relativeFilePath:string ): string {
+  const mappings: PathMapping[] = vscode.workspace.getConfiguration( "cfml-component-paths" ).get( "pathMappings", [] ) as PathMapping[];
+  
+  for ( const mapping of mappings ) {
+    
+
+    if ( relativeFilePath.startsWith( mapping.source ) ) {
+      // This might not be correct, just have to replace the start
+      return mapping.target + relativeFilePath.slice( mapping.source.length );
+    }
+  }
+  return relativeFilePath;
+}
+
 
 /**
  * Converts a CFML component file path into a native reference for CFML.
@@ -24,56 +59,88 @@ export function isTextEligibleForCodeCompletion(text: string): boolean {
  * Example:
  * 		/path/to/my/component/HereItIs.cfc => path.to.my.component.HereItIs
  */
-export function componentPathToNativeReference(workspacePath: string, filePath: string) : string {
-	return filePath.replace(workspacePath, "")
-		.replace(/^[\/|\\]/, "") // remove prefixed forward/back slashes
-		.replace(/[\/|\\]/g, ".") // replace forward/back slashes with period (.)
-		.replace(/\.[A-Za-z]+$/, ""); // remove file extension
+export function componentPathToNativeReference(
+  workspacePath: string,
+  filePath: string
+): string {
+
+	
+  let relativeFilePath = filePath.replace(workspacePath, "");
+    relativeFilePath = applyPathMappings(relativeFilePath);
+
+  return relativeFilePath
+    .replace(/^[\/|\\]/, "") // remove prefixed forward/back slashes
+    .replace(/[\/|\\]/g, ".") // replace forward/back slashes with period (.)
+    .replace(/\.[A-Za-z]+$/, ""); // remove file extension
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	// Allow code completion on all CFML files
-	const documentSelector: vscode.DocumentSelector = {
-		pattern: '**/*.{cfc,cfm}',
-		scheme: 'file',
-	};
+  // Allow code completion on all CFML files
+  const documentSelector: vscode.DocumentSelector = {
+    pattern: "**/*.{cfc,cfm}",
+    scheme: "file",
+  };
 
-	// Activate code completion
-	vscode.languages.registerCompletionItemProvider(documentSelector, {
-		async provideCompletionItems(document, position) {
-			// Get all characters of target line before the current character
-			const beforeText = document.lineAt(position.line).text.substring(0, position.character).slice(0, -1);
+  // Activate code completion
+  vscode.languages.registerCompletionItemProvider(documentSelector, {
+    async provideCompletionItems(document, position) {
+      // Get all characters of target line before the current character
+      const beforeText = document
+        .lineAt(position.line)
+        .text.substring(0, position.character)
+        .slice(0, -1);
 
-			// Get the file path of the current workspace
-			const workspacePath = vscode.workspace.getWorkspaceFolder(document.uri)?.uri.path;
+      // Get the file path of the current workspace
+      const workspacePath = vscode.workspace.getWorkspaceFolder(document.uri)
+        ?.uri.path;
 
-			if (!isTextEligibleForCodeCompletion(beforeText)) return;
-			if (!workspacePath) return;
+      if (!isTextEligibleForCodeCompletion(beforeText)) {
+        return;
+      }
+      if (!workspacePath) {
+        return;
+      }
 
-			const cfmlComponentUris = await vscode.workspace.findFiles('**/*.cfc');
+      const cfmlComponentUris = await vscode.workspace.findFiles("**/*.cfc");
 
-			return cfmlComponentUris.map(uri => {
-				const nativeReference = componentPathToNativeReference(workspacePath, uri.path);
-				const completionItem = new vscode.CompletionItem(nativeReference, vscode.CompletionItemKind.Class);
+      return cfmlComponentUris.map((uri) => {
+        const nativeReference = componentPathToNativeReference(
+          workspacePath,
+          uri.path
+        );
+        const completionItem = new vscode.CompletionItem(
+          nativeReference,
+          vscode.CompletionItemKind.Class
+        );
 
-				completionItem.detail = "(component)";
+        completionItem.detail = "(component)";
 
-				return completionItem;
-			});
-		}
-	});
+        return completionItem;
+      });
+    },
+  });
 
-	// Activate copy CF path command
-	context.subscriptions.push(vscode.commands.registerCommand('cfml-component-paths.copyPath', (selectedFileUri: vscode.Uri) => {
-		const workspace = vscode.workspace.getWorkspaceFolder(selectedFileUri);
-		const workspacePath = workspace?.uri.path;
+  // Activate copy CF path command
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "cfml-component-paths.copyPath",
+      (selectedFileUri: vscode.Uri) => {
+        const workspace = vscode.workspace.getWorkspaceFolder(selectedFileUri);
+        const workspacePath = workspace?.uri.path;
 
-		if (!workspacePath) return;
+        if (!workspacePath) {
+          return;
+        }
 
-		const nativeReference = componentPathToNativeReference(workspacePath, selectedFileUri.path);
+        const nativeReference = componentPathToNativeReference(
+          workspacePath,
+          selectedFileUri.path
+        );
 
-		vscode.env.clipboard.writeText(nativeReference);
-	}));
+        vscode.env.clipboard.writeText(nativeReference);
+      }
+    )
+  );
 }
 
 export function deactivate() {}
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-
+import * as sinon from 'sinon';
 import * as vscode from 'vscode';
 import * as myExtension from '../../extension';
 
@@ -23,5 +23,23 @@ suite('Extension Test Suite', () => {
 	test('componentPathToNativeReference', () => {
 		assert.strictEqual(myExtension.componentPathToNativeReference("/c/path/to/project", "/c/path/to/project/MyComponent.cfc"), "MyComponent");
 		assert.strictEqual(myExtension.componentPathToNativeReference("/c/path/to/project", "/c/path/to/project/another/MyComponent.cfc"), "another.MyComponent");
+	});
+	
+	test('componentPathToNativeReference with mappings path', async () => {
+		// Mapping from nomap -> mapped
+		// Mock vscode.workspace.getConfiguration
+		const mockUpdate = sinon.stub();
+		const mockGetConfiguration = sinon.stub(vscode.workspace, 'getConfiguration').returns({
+			update: mockUpdate,
+			get: sinon.stub().returns([
+				{
+					source: "/nomap",
+					target: "/mapped"
+				}
+			])
+		} as any);
+
+		assert.strictEqual(myExtension.componentPathToNativeReference("/c/path/to/project", "/c/path/to/project/nomap/MyComponent.cfc"), "mapped.MyComponent");
+		assert.strictEqual(myExtension.componentPathToNativeReference("/c/path/to/project", "/c/path/to/project/nomap/another/MyComponent.cfc"), "mapped.another.MyComponent");
 	});
 });
